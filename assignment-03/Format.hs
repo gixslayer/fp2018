77d9316788844b658c9f9cb62ba874e39f9bc779
Ciske H <ciske_harsema_2@hotmail.com>
2018-09-16 19:04:53 +0200

    More progress on assignment set 3

--- assignment-03/Format.hs
+++ assignment-03/Format.hs
@@ -3,3 +3,35 @@ where
 import Prelude hiding (Word)
 import WordList (Word, lorem)
 
+-- Take words as long as the line quota 'n' is not depleted. The function 
+-- returns a pair of word lists, where the first word list is the formatted
+-- line, and the second word list is the remaining words that didn't fit.
+formatLine :: Int -> [Word] -> ([Word], [Word])
+formatLine _ [] = ([], [])
+formatLine n (w:ws)
+    | wl <= n   = let (f, s) = formatLine (n - wl) ws in (w : f, s)
+    | otherwise = ([], w:ws)
+        where wl = length w + 1
+
+format :: Int -> [Word] -> [[Word]]
+format _ [] = []
+format n ws = line : format n rem
+    where (line, rem) = formatLine n ws
+
+-- product of putStr $ unlines $ map unwords $ format 40 $ words lorem
+--Lorem ipsum dolor sit amet, consetetur
+--sadipscing elitr, sed diam nonumy
+--eirmod tempor invidunt ut labore et
+--dolore magna aliquyam erat, sed diam
+--voluptua. At vero eos et accusam et
+--justo duo dolores et ea rebum. Stet
+--clita kasd gubergren, no sea takimata
+--sanctus est Lorem ipsum dolor sit amet.
+--Lorem ipsum dolor sit amet, consetetur
+--sadipscing elitr, sed diam nonumy
+--eirmod tempor invidunt ut labore et
+--dolore magna aliquyam erat, sed diam
+--voluptua. At vero eos et accusam et
+--justo duo dolores et ea rebum. Stet
+--clita kasd gubergren, no sea takimata
+--sanctus est Lorem ipsum dolor sit amet.
--- assignment-03/QuickTest.hs
+++ assignment-03/QuickTest.hs
@@ -14,16 +14,57 @@ infixr 1  -->, ==>
 probes --> prop  =  \ f -> and [ prop (f x) | x <- probes ]
 probes ==> prop  =  \ f -> and [ prop x (f x) | x <- probes ]
 
--- ordered      :: (Ord a) => Property [a]
--- permutations :: [a] -> Probes [a]
+-- 3.5.1
+ordered :: (Ord a) => Property [a]
+ordered [] = True
+ordered (x:[]) = True
+ordered (x:y:xs) = x <= y && ordered (y:xs)
 
+-- 3.5.2
+removeAt :: Int -> [a] -> [a]
+removeAt i xs | i < length xs = take i xs ++ drop (i+1) xs
+
+permutations :: [a] -> Probes [a]
+permutations [] = [[]]
+permutations xs = [ (xs !! i) : ys | i <- [0..(length xs)-1], ys <- permutations (removeAt i xs) ]
+
+-- 3.5.3
+runs :: (Ord a) => [a] -> [[a]]
+runs [x] = [[x]]
+runs (x:y:xs) 
+    | x <= y    = (x : head ys) : drop 1 ys
+    | otherwise = [x] : ys
+        where ys = runs (y:xs)
+
+runProbes :: Probes String
+runProbes = ["hello, world!\n", "test", "abcdcba"]
+
+runProp :: Property [String]
+runProp = and . map ordered
+
+testRuns :: (String -> [String]) -> Bool
+testRuns = runProbes --> runProp
+
+-- Finally, run 'testRuns runs' and see if it produces True.
+-- Alternatively, permutations [1..5] --> and . map ordered $ runs
+
+-- 3.5.4
 isqrt :: Integer -> Integer
 isqrt n = loop 0 3 1
   where loop i k s  | s <= n      = loop (i + 1) (k + 2) (s + k)
                     | otherwise  = i
 
--- infixr 4  ⊗
--- (⊗) :: Probes a -> Probes b -> Probes (a, b)
+isIntegerSqrt :: Property (Integer -> Integer)
+isIntegerSqrt = map (\n -> n * n) [0..10] ==> \x y -> y*y == x
+
+-- Then test with isIntegerSqrt isqrt, or test with the build in sqrt as
+-- isIntegerSqrt (floor . sqrt . fromIntegral)
+
+-- 3.5.5
+infixr 4 ***
+(***) :: Probes a -> Probes b -> Probes (a, b)
+(***) as bs = [(a,b) | a <- as, b <- bs]
+-- Produces m*n elements where m = |as|, n = |bs|
 
 niftySort :: [a] -> [a]
 niftySort _xs  =  []
