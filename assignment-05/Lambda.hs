4be0671442bf247c53a6da4c34bf867a331651b9
gixslayer <ciske_harsema_2@hotmail.com>
2018-10-05 14:56:19 +0200

    Added 5.4 and non unicode Parser

--- assignment-05/Lambda.hs
+++ assignment-05/Lambda.hs
@@ -1,12 +1,49 @@
 module Lambda
 where
-import Prelude hiding (fail)
+import Prelude
 import Parser
+
+-- 5.4.1
+
+{-
+lambda          ::= {term ' '} term
+                  | '\' var '->' lambda
+term            ::= var
+                  | '(' lambda ')'
+var             ::= idStart {idMid}
+idStart         ::= lower | upper | '_'
+idMid           ::= idStart | digit | "'"
+
+Does not support λ/.-syntax
+Does not accept whitespace in other locations than function application,
+  in which case whitespace (one space) is required
+-}
 
 infixl 9 :@
 
 data Lambda var
   =  Var var                   -- variable
-  |  Fun var (Lambda var)      -- abstraction/lambda-expression
+  |  Fun var (Lambda var)      -- abstraction/λ-expression
   |  Lambda var :@ Lambda var  -- application
   deriving (Show)
+
+lambda, term :: Parser (Lambda String)
+lambda  = (many (term >>= \t -> symbol ' ' >> return t) >>= \ts -> term >>= \t -> return $ foldr (:@) t ts)
+       .| (symbol '\\' >> var >>= \v -> symbol '-' >> symbol '>' >> lambda >>= \l -> return $ Fun v l)
+term    = (var >>= \v -> return $ Var v)
+       .| (symbol '(' >> lambda >>= \l -> symbol ')' >> return l)
+
+var :: Parser String
+var     = idStart >>= \s -> many idMid >>= \i -> return $ s : i
+
+idStart, idMid :: Parser Char
+idStart = lower .| upper .| symbol '_'
+idMid   = idStart .| digit .| symbol '\''
+
+
+-- 5.4.2
+
+-- parse lambda "x (\\a->b (\\z->(y))) (Henk42_')"
+-- Var "x" :@ (Fun "a" (Var "b" :@ Fun "z" (Var "y")) :@ Var "Henk42_'")
+
+-- a\x->x is not accepted. which is correct
