7b6d278687820ae229975afaa730815b9920a06c
Ciske H <ciske_harsema_2@hotmail.com>
2018-12-11 22:01:52 +0100

    Still don't understand what the hell I'm doing

--- assignment-12/Printf.hs
+++ assignment-12/Printf.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveFunctor #-}
 
 module Printf
 where
@@ -21,12 +25,15 @@ printf dir = format dir id ""
 class Format dir where
   format :: dir -> (String -> a) -> String -> Arg dir a
 
+newtype (f :.: g) x = TC { fromTC :: f (g x) }
+  deriving (Functor)
+
 -- 12.4
 type instance Arg D res = Int -> res
 type instance Arg F res = Float -> res
 type instance Arg S res = String -> res
 type instance Arg String res = res
---type instance Arg (a, b) res = Arg a res -> Arg b res -- probably wrong
+type instance Arg (a, b) res = Arg a res -> Arg b res -> res -- probably wrong
 
 instance Format D where
   format D cont out = \i -> cont (out ++ show i)
@@ -40,7 +47,11 @@ instance Format S where
 instance Format String where
   format s cont out = cont (out ++ s)
 
---instance (Format dir1,Format dir2) => Format (dir1, dir2) where
+instance (Format dir1,Format dir2) => Format (dir1, dir2) where
+  format (d1, d2) cont out = undefined
+
+combine :: (Functor f, Functor g) => f String -> g String -> f ( g String )
+combine fs gs = fmap (\s -> fmap (\t -> s ++ t) gs) fs
 
 --printf D 51
 --printf ("I am " & D & " years old.") 51
